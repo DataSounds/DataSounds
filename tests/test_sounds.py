f1de59595855535846a3102fc467487144561100
Luiz Irber <luiz.irber@gmail.com>
2013-08-12 18:28:37 -0300

    Improved test coverage

--- tests/test_sounds.py
+++ tests/test_sounds.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from DataSounds.sounds import build_scale
+import numpy as np
+
+
+from DataSounds.sounds import build_scale, note_number, note_name
 
 
 def test_build_scale_major():
@@ -11,3 +14,19 @@ def test_build_scale_major():
 def test_build_scale_minor():
     scale = build_scale('A', 'minor', 1)
     assert any('is' in note for note in scale) is False
+
+
+def test_build_scale_pentatonic():
+    scale = build_scale('C', 'pentatonic', 1)
+    assert scale == ['c', 'd', 'e', 'g', 'a']
+
+
+def test_note_number():
+    scale = build_scale('C', 'major', 1)
+    assert all(note_number([1, 2, 3, 4], scale) == [0, 2, 4, 6])
+
+
+def test_build_note_name():
+    scale = build_scale('C', 'major', 1)
+    notes = note_number([1, 2, 3, 4, np.nan], scale)
+    assert [note_name(x, scale) for x in notes] == ['c', 'e', 'g', 'b', 'r']
