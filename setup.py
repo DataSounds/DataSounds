9129a0984331a65bd86477bb1401b2373abede2d
Luiz Irber <luiz.irber@gmail.com>
2015-02-04 17:02:13 -0500

    Bump version: 1.2.3 → 1.2.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-version = '1.2.3'
+version = '1.2.4'
 
 install_requires = [
 #    'sebastian',
